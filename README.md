d0b4e4f339ced89153121fc976cbe52f07765b3b
himath <himathcode100@gmail.com>
2025-07-21 10:45:42 +0530

    update files structure

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 ![](https://upload.wikimedia.org/wikipedia/commons/f/f4/Raylib_logo.png)
 
-# 🎮 Introduction to raylib
+> # Introduction to raylib
 
 - raylib is a simple and easy-to-use C library to learn game programming. It is designed to be highly readable and beginner-friendly, making it perfect for creating 2D and 3D games while learning the fundamentals of C/C++ and graphics programming.
 
 🔗 Official Website: https://www.raylib.com
 🔗 GitHub Repository: https://github.com/raysan5/raylib
 
-## ⚙️ Manual Setup (Windows using W64Devkit)
+> ## Manual Setup (Windows using W64Devkit)
 
 - This guide will help you manually set up raylib with W64Devkit, a minimalist GCC toolchain for Windows.
 
-### 📥 Step 1: Download W64Devkit
+> ### Step 1: Download W64Devkit
 - Go to: https://github.com/skeeto/w64devkit 
 - Download the latest w64devkit-x.xx.x.zip
 
@@ -20,7 +20,7 @@ Extract it to:
 C:\w64devkit
 ```
 
-### 📥 Step 2: Download raylib for MinGW
+> ### Step 2: Download raylib for MinGW
 - Go to: https://github.com/raysan5/raylib/releases
 - Download: raylib-5.0_win64_mingw-w64.zip
 - Extract the include/ and lib/ folders into:
@@ -29,19 +29,19 @@ C:\w64devkit\x86_64-w64-mingw32
 ```
 
 
-### 🚀 Step 3: Launch the Compiler
+> ### Step 3: Launch the Compiler
 - Open C:\w64devkit
 
 - Run w64devkit.exe — this opens a terminal window with GCC ready.
 
-### 🛠️ Step 4: Compile a raylib Program
+> ### Step 4: Compile a raylib Program
 - In the terminal, compile your .c file like this:
 
 ```
 gcc -o pixel1.exe  pixel.cpp -lraylib -lgdi32 -lwinmm
 ```
 
-### 🛠️️️ step 5: Run the program
+> ### step 5: Run the program
 - run the exe file
 
 ```
--- pixel.cpp
+++ pixel.cpp
@@ -1,27 +1,40 @@
 #include <raylib.h>
 
 int main(void){
-    InitWindow(600,500,"Pixel game");
+    InitWindow(700,600,"Pixel game");       // width,height
     
     SetTargetFPS(60);
 
-    Rectangle rec={0,480,20,40}; // x , y , width , height
+    Rectangle rec={0,580,20,20}; // x , y , width , height
     int speed=10; // player speed
+
     bool isJumping = false; // flag to track jumping
     int jumpSpeed = -15; // initial jump speed
     int gravity = 1; // gravity effect
     int velocityY = 0; // vertical velocity
 
+    // Camera2D camera = { 0 };
+    // camera.target = (Vector2){   };      
+    // camera.offset = (Vector2){ GetScreenWidth()/2, GetScreenHeight()/2 };  
+    // camera.rotation = 0.0f;                      
+    // camera.zoom = 1.0f;  
+
     while(!WindowShouldClose()){
 
-        // Horizontal movement
-        if(IsKeyDown(KEY_RIGHT) && rec.x+rec.width<GetScreenWidth()){ // move right
+
+        if(IsKeyDown(KEY_RIGHT) && rec.x<GetScreenWidth()){ 
             rec.x+=speed;
+            // if(rec.x>GetScreenWidth()/2){
+                
+            // }
+            // camera.target.x-=2;
         }
-        if(IsKeyDown(KEY_LEFT) && rec.x>0){ // move left
+        if(IsKeyDown(KEY_LEFT) && rec.x>0){ 
             rec.x-=speed;
+            // camera.target.x+=2;
         }
 
+        
         // Jump logic
         if(IsKeyPressed(KEY_SPACE) && !isJumping){ // start jump
             isJumping = true;
@@ -33,8 +46,8 @@ int main(void){
             velocityY += gravity; // apply gravity
 
             // Stop jumping when rectangle lands
-            if(rec.y > 480){
-                rec.y = 480; // reset to ground level
+            if(rec.y > 580){
+                rec.y= 580; // reset to ground level
                 isJumping = false;
                 velocityY = 0;
             }
@@ -43,6 +56,8 @@ int main(void){
         BeginDrawing();
         ClearBackground(BLACK);
         DrawRectangleRec(rec,WHITE);
+
+        TraceLog(LOG_INFO, "rec.y: %f, velocityY: %d", rec.y, rec.height);
 
         EndDrawing();        
     }
